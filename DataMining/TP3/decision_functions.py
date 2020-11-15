201d7a3ec2250aad2dae3276d8fba71893051d82
bourhan dernayka <hanodern@gmail.com>
2020-11-15 21:07:42 +0100

    start pack of TP3

--- DataMining/TP3/decision_functions.py
+++ DataMining/TP3/decision_functions.py
@@ -1,5 +1,13 @@
-def BuildDecisionTree():
-	pass
+def BuildDecisionTree(path, minNum, default):
+    df=pd.read_csv(path)
+    df=df[:20]
+    A=list(df.columns[:-1])
+    classifier=df.columns[-1]
+    D = df
+    default = 0
+    tree= createNode(D, A, "Root", 0, classifier, minNum, default)
+    continueTree(tree)
+    return tree
 
 def printDecisionTree():
 	pass
--- DataMining/TP3/main.py
+++ DataMining/TP3/main.py
@@ -3,5 +3,16 @@ import pandas as pd
 import decision_functions
 
 def main():
-    print("hi")
+    alpha=0.5; minNum = 5; dataPath = 'data.csv'; defaultClassifierValue=1;
     
+    tree = BuildDecisionTree(dataPath, minNum, defaultClassifierValue)
+    
+    printDecisionTree(tree)
+    
+    error = generalizationError(tree, alpha)
+    print("The generalization error is",error)
+    
+    prunedTree = pruneTree(mytree, classifier, alpha)
+    errorPruned=generalizationError(prunedTree, alpha)
+    print("The generalization error of the pruned tree is",errorPruned,end="\n\n")
+    printDecisionTree(prunedTree)
